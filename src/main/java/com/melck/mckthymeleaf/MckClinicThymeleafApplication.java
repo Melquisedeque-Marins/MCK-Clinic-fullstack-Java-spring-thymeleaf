58bed89e8c5712d2d1e13cc588c22783971961c8
melck <melck_junior@hotmail.com>
2022-08-23 09:38:41 +0100

    First commit

---
+++ src/main/java/com/melck/mckthymeleaf/MckClinicThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.melck.mckthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MckClinicThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MckClinicThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melck/mckthymeleaf/MckClinicThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melck.mckthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MckClinicThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
